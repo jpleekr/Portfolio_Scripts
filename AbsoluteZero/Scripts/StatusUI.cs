5b5d82271847991516ee52c4e283f56dfbc2164f
jpleekr <109937740+jpleekr@users.noreply.github.com>
2026-08-04 12:21:49 +0900

    Refactor Update method in StatusUI class
    Refactored Update method to remove redundancy and improve clarity.

--- AbsoluteZero/Scripts/StatusUI.cs
+++ AbsoluteZero/Scripts/StatusUI.cs
@@ -3,22 +3,25 @@ using UnityEngine.UI;
 
 public class StatusUI : MonoBehaviour
 {
+	// 플레이어 상태 UI
 	[SerializeField] private Image uiPlayerHpBar;
 	[SerializeField] private Image uiPlayerHungerBar;
 	[SerializeField] private Image uiPlayerTirstBar;
 	[SerializeField] private Image uiPlayerMentalityBar;
 	[SerializeField] private Image uiPlayerColdBar;
 
-
-    void Update()
-    {
-        UpdateUI();
-    }
+	void Update()
+	{
+		// 플레이어 상태에 맞게 UI 갱신
+		UpdateUI();
+	}
 
 	private void UpdateUI()
 	{
+		// PlayerStatusManager가 아직 생성되지 않았으면 종료
 		if (PlayerStatusManager.Instance == null) return;
 
+		// 각 스탯의 비율을 UI에 반영
 		uiPlayerHpBar.fillAmount = PlayerStatusManager.Instance.CurrentHpPercent;
 		uiPlayerHungerBar.fillAmount = PlayerStatusManager.Instance.CurrentHungerPercent;
 		uiPlayerTirstBar.fillAmount = PlayerStatusManager.Instance.CurrentThirstPercent;
